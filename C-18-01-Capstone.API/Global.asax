efa6f1b7b23615e64ace3fb4cbb6daf42e544d1a
SU\vlad.medvedovskiy <vlad.medvedovskiy@sigma.software>
2018-04-06 15:49:47 +0300

    Sample web api project

---
+++ C-18-01-Capstone.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="C_18_01_Capstone.API.WebApiApplication" Language="C#" %>
---
+++ C-18-01-Capstone.API/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace C_18_01_Capstone.API
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+    }
+}
