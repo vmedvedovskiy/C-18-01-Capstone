9db10259d9f12146b924e697d096a2e1ec583d14
Olga Levchenko <olga.kazakovskaya@dataart.com>
2018-04-16 23:44:30 +0300

    Add empty web project with UserController

---
+++ C-18-01-Capstone.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using C_18_01_Capstone.Web.ViewModels;
+
+namespace C_18_01_Capstone.Web.Controllers
+{  
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Login(LoginViewModel loginViewModel)
+        {
+          return "Logged in!";
+        }
+    }
+}
