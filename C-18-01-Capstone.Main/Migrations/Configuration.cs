ad1040a78f447f2e8ee27c3f55e7955b0abea4a7
YevheniiTk <vkstrelok@gmail.com>
2018-04-20 15:11:27 +0300

    dbInitializer.InitializeCountriesTable();

--- C-18-01-Capstone.Main/Migrations/Configuration.cs
+++ C-18-01-Capstone.Main/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace C_18_01_Capstone.Main.Migrations
 {
+    using C_18_01_Capstone.Main.DataContext;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +16,8 @@ namespace C_18_01_Capstone.Main.Migrations
 
         protected override void Seed(C_18_01_Capstone.Main.DataContext.SocialNetworkContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            DbInitializer dbInitializer = new DbInitializer();
+            dbInitializer.InitializeCountriesTable();
         }
     }
 }
