4ea95481c6cd498d2d57096e53a00f20d828f9f7
YevheniiTk <vkstrelok@gmail.com>
2018-04-20 11:55:36 +0300

    formatting style was changed

--- C-18-01-Capstone.Services.Implementation/Services/EncryptionService.cs
+++ C-18-01-Capstone.Services.Implementation/Services/EncryptionService.cs
@@ -6,26 +6,26 @@ using C_18_01_Capstone.Services.Services;
 namespace C_18_01_Capstone.Services.Implementation.Services
 {
     public class EncryptionService : IEncryptionService
-  {
-    public string CreateSalt()
     {
-      var data = new byte[0x10];
-      using (var cryptoServiceProvider = new RNGCryptoServiceProvider())
-      {
-        cryptoServiceProvider.GetBytes(data);
-        return Convert.ToBase64String(data);
-      }
-    }
+        public string CreateSalt()
+        {
+            var data = new byte[0x10];
+            using (var cryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                cryptoServiceProvider.GetBytes(data);
+                return Convert.ToBase64String(data);
+            }
+        }
 
-    public string EncryptPassword(string password, string salt)
-    {
-      using (var sha256 = SHA256.Create())
-      {
-        var saltedPassword = String.Format("{0}{1}", salt, password);
-        byte[] saltedPasswordAsBytes = Encoding.UTF8.GetBytes(saltedPassword);
-        var hashedBytes = sha256.ComputeHash(saltedPasswordAsBytes);
-        return Convert.ToBase64String(hashedBytes);
-      }
+        public string EncryptPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = String.Format("{0}{1}", salt, password);
+                byte[] saltedPasswordAsBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                var hashedBytes = sha256.ComputeHash(saltedPasswordAsBytes);
+                return Convert.ToBase64String(hashedBytes);
+            }
+        }
     }
-  }
 }
